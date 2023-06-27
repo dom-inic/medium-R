5229c505cfe9d1f748b2eb77207ae78bcee088ca
dom-inic <dominicnyambane22@gmail.com>
2023-06-27 13:58:03 +0300

    comments

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.views import generic
 from django.core.mail import send_mail
-from . models import Post
-from . forms import EmailPostForm
+from . models import Post, Comment
+from . forms import EmailPostForm, CommentForm
 # Create your views here.
 
 def post_list(request):
@@ -29,7 +29,25 @@ class PostList(generic.ListView):
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
-    return render(request, 'blog/post/post_detail.html', {'post': post})
+
+    # list or active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # create comment object but dont save to database yet
+            new_comment = comment_form.save(commit=False)
+            # assign the current post to the comment
+            new_comment.post = post
+            # save comment to the database 
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/post_detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment,
+                                                          'comment_form': comment_form})
 
 def post_share(request, post_id):
     # retrieve post by id
@@ -44,7 +62,7 @@ def post_share(request, post_id):
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url} \n\n {cd['name']}'s comments: {cd['comments']}"
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            send_mail(subject, message, 'admin@medium-R.com', [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
